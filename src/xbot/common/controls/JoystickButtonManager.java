de5e3cf386efd41c81cb57fac7acbcf57e780713
Alex Schokking <aschokking@gmail.com>
2015-10-18 07:44:22 -0700

    Fix formatting on joystick button manager files

--- src/xbot/common/controls/JoystickButtonManager.java
+++ src/xbot/common/controls/JoystickButtonManager.java
@@ -12,55 +12,50 @@ import xbot.common.wpi_extensions.mechanism_wrappers.XJoystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
 /**
- * Ensures that joystick buttons are only used once and not assigned to multiple commands by
- * accident.
+ * Ensures that joystick buttons are only used once and not assigned to multiple commands by accident.
  */
 public class JoystickButtonManager {
 
-	private static final Logger log = Logger
-			.getLogger(JoystickButtonManager.class);
+    private static final Logger log = Logger.getLogger(JoystickButtonManager.class);
 
-	private HashMap<Integer, AdvancedJoystickButton> buttonMap;
-	private HashMap<AnalogHIDButton.AnalogHIDDescription, AnalogHIDButton> analogButtonMap;
-	
-	private WPIFactory factory;
-	
-	private XJoystick joystick;
+    private HashMap<Integer, AdvancedJoystickButton> buttonMap;
+    private HashMap<AnalogHIDButton.AnalogHIDDescription, AnalogHIDButton> analogButtonMap;
 
-	public JoystickButtonManager(int numButtons, WPIFactory factory,
-			XJoystick joystick) {
-	    this.joystick = joystick;
-	    this.factory = factory;
-	    
-		this.buttonMap = new HashMap<Integer, AdvancedJoystickButton>(numButtons);
-		this.analogButtonMap = new HashMap<>();
-		
-		for (int i = 1; i <= numButtons; i++) {
-			this.set(i, factory.getJoystickButton(joystick, i));
-		}
-	}
-	
-	public void addAnalogButton(int axisNumber, double minThreshold, double maxThreshold)
-	{
-	    addAnalogButton(new AnalogHIDDescription(axisNumber, minThreshold, maxThreshold));
-	}
-	
-	public void addAnalogButton(AnalogHIDDescription desc)
-    {
+    private WPIFactory factory;
+
+    private XJoystick joystick;
+
+    public JoystickButtonManager(int numButtons, WPIFactory factory, XJoystick joystick) {
+        this.joystick = joystick;
+        this.factory = factory;
+
+        this.buttonMap = new HashMap<Integer, AdvancedJoystickButton>(numButtons);
+        this.analogButtonMap = new HashMap<>();
+
+        for (int i = 1; i <= numButtons; i++) {
+            this.set(i, factory.getJoystickButton(joystick, i));
+        }
+    }
+
+    public void addAnalogButton(int axisNumber, double minThreshold, double maxThreshold) {
+        addAnalogButton(new AnalogHIDDescription(axisNumber, minThreshold, maxThreshold));
+    }
+
+    public void addAnalogButton(AnalogHIDDescription desc) {
         setAnalog(factory.getAnalogJoystickButton(joystick, desc));
     }
 
-	public AdvancedJoystickButton getifAvailable(int buttonNumber) {
-		if (buttonMap.containsKey(buttonNumber)) {
-			return buttonMap.remove(buttonNumber);
-		} else {
-			// Warn people that terrible things are happening, then return a null button.
-			log.error("button " + buttonNumber + " is already used! Cannot be used twice!");
-			return null;
-		}
-	}
-	
-	public Button getAnalogIfAvailable(AnalogHIDDescription desc) {
+    public AdvancedJoystickButton getifAvailable(int buttonNumber) {
+        if (buttonMap.containsKey(buttonNumber)) {
+            return buttonMap.remove(buttonNumber);
+        } else {
+            // Warn people that terrible things are happening, then return a null button.
+            log.error("button " + buttonNumber + " is already used! Cannot be used twice!");
+            return null;
+        }
+    }
+
+    public Button getAnalogIfAvailable(AnalogHIDDescription desc) {
         if (analogButtonMap.containsKey(desc)) {
             return analogButtonMap.remove(desc);
         } else {
@@ -71,13 +66,13 @@ public class JoystickButtonManager {
 
     }
 
-	// sets without checking
-	// intended only for initializing buttons from factory.
-	private void set(int buttonNumber, AdvancedJoystickButton button) {
-		buttonMap.put(buttonNumber, button);
-	}
-	
-	// sets without checking
+    // sets without checking
+    // intended only for initializing buttons from factory.
+    private void set(int buttonNumber, AdvancedJoystickButton button) {
+        buttonMap.put(buttonNumber, button);
+    }
+
+    // sets without checking
     // intended only for initializing buttons from factory.
     private void setAnalog(AnalogHIDButton button) {
         analogButtonMap.put(button.getDescription(), button);
