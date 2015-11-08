d3fc1e49666bae7aee2f19ba9074bce72fd21865
WasabiFan <wasabifan@outlook.com>
2015-11-07 17:11:59 -0800

    Remove unnecessary TODO comments

--- src/xbot/common/autonomous/ScriptedCommandFunctionBase.java
+++ src/xbot/common/autonomous/ScriptedCommandFunctionBase.java
@@ -8,108 +8,86 @@ public abstract class ScriptedCommandFunctionBase implements Function {
 
     @Override
     public void delete(String arg0) {
-        // TODO Auto-generated method stub
         
     }
 
     @Override
     public void delete(int arg0) {
-        // TODO Auto-generated method stub
         
     }
 
     @Override
     public Object get(String arg0, Scriptable arg1) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Object get(int arg0, Scriptable arg1) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public String getClassName() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.getClass().getName();
     }
 
     @Override
     public Object getDefaultValue(Class<?> arg0) {
-        try {
-            return arg0.newInstance();
-        } catch (Exception e) {
-            
-        }
-        
         return null;
     }
 
     @Override
     public Object[] getIds() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Scriptable getParentScope() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Scriptable getPrototype() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public boolean has(String arg0, Scriptable arg1) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean has(int arg0, Scriptable arg1) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean hasInstance(Scriptable arg0) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public void put(String arg0, Scriptable arg1, Object arg2) {
-        // TODO Auto-generated method stub
         
     }
 
     @Override
     public void put(int arg0, Scriptable arg1, Object arg2) {
-        // TODO Auto-generated method stub
         
     }
 
     @Override
     public void setParentScope(Scriptable arg0) {
-        // TODO Auto-generated method stub
         
     }
 
     @Override
     public void setPrototype(Scriptable arg0) {
-        // TODO Auto-generated method stub
         
     }
 
     @Override
     public Scriptable construct(Context arg0, Scriptable arg1, Object[] arg2) {
-        // TODO Auto-generated method stub
         return null;
     }
 
