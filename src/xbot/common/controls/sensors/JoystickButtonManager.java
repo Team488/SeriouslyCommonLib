6efb9109c2360db11675a07c191f2e3068e3eb02
John Gilbert <johngilb@gmail.com>
2016-02-06 15:46:06 -0800

    Move MockJoystick into source tree instead of test tree

--- src/xbot/common/controls/sensors/JoystickButtonManager.java
+++ src/xbot/common/controls/sensors/JoystickButtonManager.java
@@ -9,7 +9,6 @@ import xbot.common.controls.sensors.AnalogHIDButton.AnalogHIDDescription;
 import xbot.common.injection.wpi_factories.WPIFactory;
 import xbot.common.logging.RobotAssertionException;
 import xbot.common.logging.RobotAssertionManager;
-import edu.wpi.first.wpilibj.MockJoystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
 /**
