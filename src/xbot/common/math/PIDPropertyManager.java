837d2c53756fe69ca766b6f305aadb314b5cca13
JohnGilb <johngilb@gmail.com>
2017-02-04 14:24:15 -0800

    Feature/pid property manager lite (#47)
    * Initial creation of PID property manager
    
    * for now, just restrict to PIDF, since that's what we're constantly updating on CANTalons
    
    * No need for name variable.
    
    * removing the word "default" where it's not appropriate.

---
+++ src/xbot/common/math/PIDPropertyManager.java
@@ -0,0 +1,54 @@
+package xbot.common.math;
+
+import com.google.inject.Inject;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.XPropertyManager;
+
+public class PIDPropertyManager {
+    
+    private DoubleProperty propP;
+    private DoubleProperty propI;
+    private DoubleProperty propD;
+    private DoubleProperty propF;
+    
+    @Inject
+    public PIDPropertyManager(String functionName, XPropertyManager propMan, 
+            double defaultP, double defaultI, double defaultD, double defaultF) {
+        propP = propMan.createPersistentProperty(functionName + " P", defaultP);
+        propI = propMan.createPersistentProperty(functionName + " I", defaultI);
+        propD = propMan.createPersistentProperty(functionName + " D", defaultD);
+        propF = propMan.createPersistentProperty(functionName + " F", defaultF);
+    }
+
+    public double getP() {
+        return propP.get();
+    }
+
+    public void setP(double p) {
+        propP.set(p);
+    }
+
+    public double getI() {
+        return propI.get();
+    }
+
+    public void setI(double i) {
+        propI.set(i);
+    }
+
+    public double getD() {
+        return propD.get();
+    }
+
+    public void setD(double d) {
+        propD.set(d);
+    }
+
+    public double getF() {
+        return propF.get();
+    }
+
+    public void setF(double f) {
+        propF.set(f);
+    }
+}
