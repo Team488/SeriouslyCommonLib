b669593c49b725f4a19dfdefb91db807e507e35f
Alex Schokking <aschokking@gmail.com>
2017-02-11 18:35:38 -0800

    playing with logging changes

--- src/xbot/common/command/BaseCommand.java
+++ src/xbot/common/command/BaseCommand.java
@@ -1,5 +1,7 @@
 package xbot.common.command;
 
+import org.apache.log4j.Logger;
+
 import com.google.inject.Inject;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -10,14 +12,18 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public abstract class BaseCommand extends Command {
 
+    protected Logger log;
+    
     @Inject
     SmartDashboardCommandPutter commandPutter;
 
     public BaseCommand() {
+        log = Logger.getLogger(this.getName());
     }
 
     public BaseCommand(String name) {
         super(name);
+        log = Logger.getLogger(this.getName());
     }
 
     @Override
--- src/xbot/common/command/BaseSubsystem.java
+++ src/xbot/common/command/BaseSubsystem.java
@@ -1,15 +1,21 @@
 package xbot.common.command;
 
+import org.apache.log4j.Logger;
+
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public abstract class BaseSubsystem extends Subsystem {
+    
+    protected Logger log;
 
     public BaseSubsystem() {
+        log = Logger.getLogger(this.getName());
     }
 
     public BaseSubsystem(String name) {
         super(name);
+        log = Logger.getLogger(this.getName());
     }
     
     public void setDefaultCommand(Command command) {
