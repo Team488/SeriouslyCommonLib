821a59323dd21f258a64a4cd30ff1ac475c9c634
tfrancisco <tfrancisco@forestridge.org>
2017-02-16 18:45:58 -0800

    move double property timeout to constructor

--- src/xbot/common/command/TimeoutCommand.java
+++ src/xbot/common/command/TimeoutCommand.java
@@ -6,8 +6,8 @@ import xbot.common.properties.XPropertyManager;
 public class TimeoutCommand extends BaseCommand{
     DoubleProperty timeout;
     
-    public TimeoutCommand(XPropertyManager propManager){
-        timeout = propManager.createEphemeralProperty("initialTimeout", 0);
+    public TimeoutCommand(DoubleProperty timeout){
+        this.timeout = timeout;
     }
 
     @Override
@@ -20,15 +20,6 @@ public class TimeoutCommand extends BaseCommand{
     
     public boolean isFinished(){
         return this.isTimedOut();
-    }
-    
-    /**
-     * Allows you to set a timeout with a double property. (Allows easy tuning)
-     * @param timeout
-     *      the timeout as a double property
-     */
-    public void setTimeoutProperty(DoubleProperty timeout){
-        this.timeout = timeout;
     }
 
 }
