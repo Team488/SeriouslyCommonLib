a0729c4f39f09e9e47821c5fffd8a97a0e33c885
JohnGilb <johngilb@gmail.com>
2017-02-20 14:11:33 -0800

    Feature/isontarget pid logic (#73)
    * Unfinished work (2/18/17)
    
    * Extended PIDManager to have logic for isOnTarget for time.
    
    * implement a working time check
    
    * align variable names for time

--- tests/xbot/common/math/PIDManagerTest.java
+++ tests/xbot/common/math/PIDManagerTest.java
@@ -7,17 +7,21 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.wpi.first.wpilibj.MockTimer;
+import xbot.common.controls.sensors.XTimer;
 import xbot.common.injection.BaseWPITest;
 import xbot.common.logging.RobotAssertionException;
 
-public class PIDManagerTest extends BaseWPITest{
+public class PIDManagerTest extends BaseWPITest {
     
     PIDFactory factory;
+    MockTimer mockTimer;
     
     @Before
     public void setUp() {
         super.setUp();
         factory = injector.getInstance(PIDFactory.class);
+        mockTimer = injector.getInstance(MockTimer.class);
     }
     
     @Test
@@ -42,13 +46,13 @@ public class PIDManagerTest extends BaseWPITest{
     
     @Test
     public void testIsOnTargetStartsFalse() {
-        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0);
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0, -1);
         assertFalse(manager.isOnTarget());
     }
     
     @Test
     public void testIsOnTargetUsingError() {
-        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0);
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0, -1);
         
         manager.calculate(100, 0);
         assertFalse(manager.isOnTarget());
@@ -59,7 +63,7 @@ public class PIDManagerTest extends BaseWPITest{
     
     @Test
     public void testIsOnTargetUsingDerivative() {
-        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 0, 1);
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 0, 1, -1);
         
         manager.calculate(100, 0);
         assertFalse(manager.isOnTarget());
@@ -73,7 +77,7 @@ public class PIDManagerTest extends BaseWPITest{
     
     @Test
     public void testIsOnTargetUsingErrorAndDerivative() {
-        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 1);
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 1, -1);
         
         manager.calculate(100, 0);
         assertFalse(manager.isOnTarget());
@@ -87,7 +91,7 @@ public class PIDManagerTest extends BaseWPITest{
     
     @Test
     public void testIsOnTargetThenNot() {
-        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0);
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0, -1);
         manager.calculate(100, 0);
         assertFalse(manager.isOnTarget());
         
@@ -120,14 +124,14 @@ public class PIDManagerTest extends BaseWPITest{
     
     @Test(expected=RobotAssertionException.class)
     public void testAttemptNegativeThreshold() {
-        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 1);
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 1, -1);
         
         manager.setErrorThreshold(-10);
     }
     
     @Test
     public void disableEnableErrorTolerance() {
-        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0);
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0, -1);
         
         manager.calculate(100, 100);
         
@@ -146,7 +150,7 @@ public class PIDManagerTest extends BaseWPITest{
     
     @Test
     public void disableEnableDerivativeTolerance() {
-        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 0, 1);
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 0, 1, -1);
         
         manager.calculate(100, 100);
         manager.calculate(100, 100);
@@ -186,5 +190,54 @@ public class PIDManagerTest extends BaseWPITest{
         
         output = manager.calculate(100, 0);
         assertEquals(0.3 * 3, output, 1e-6);
+    }
+    
+    @Test
+    public void testSimpleTimeThresholding() {
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0, 1);
+        // start with large error
+        manager.calculate(100, 0);
+        assertFalse(manager.isOnTarget());
+        
+        // get within error bounds, but no time has passed, so should not be on target
+        manager.calculate(100, 100);
+        assertFalse(manager.isOnTarget());
+        
+        // advance time a little bit, but not the full second, so should still be off target
+        mockTimer.setTimeInSeconds(.75);
+        manager.calculate(100, 100);
+        assertFalse(manager.isOnTarget());
+        
+        // advance time past the 1 second mark, so should finally report on target.
+        mockTimer.setTimeInSeconds(1.1);
+        manager.calculate(100, 100);
+        assertTrue(manager.isOnTarget());
+    }
+    
+    @Test
+    public void testTimeThresholdOnTargetThenOffTargetThenOnTarget() {
+        PIDManager manager = factory.createPIDManager("test", 1, 0, 0, 0, 0.5, -0.25, 1, 0, 1);
+        
+        manager.calculate(100, 0);
+        assertFalse(manager.isOnTarget());
+        
+        // Get within error threshold, and for some time
+        manager.calculate(100, 100);
+        mockTimer.setTimeInSeconds(1.5);
+        manager.calculate(100, 100);
+        assertTrue(manager.isOnTarget());
+        
+        // Lose error threshold, should no longer be on target
+        manager.calculate(100, 50);
+        assertFalse(manager.isOnTarget());
+        
+        // get within error threshold, should still be off target
+        manager.calculate(100, 100);
+        assertFalse(manager.isOnTarget());
+        
+        // then wait a bit
+        mockTimer.setTimeInSeconds(3.0);
+        manager.calculate(100, 100);
+        assertTrue(manager.isOnTarget());
     }
 }
