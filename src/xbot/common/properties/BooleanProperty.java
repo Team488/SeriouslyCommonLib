214680dd1879d3d3dcb923f30bab04b7d04381d8
WasabiFan <wasabifan@outlook.com>
2017-02-25 10:31:50 -0800

    Remove unnecessary import and enhance message

--- src/xbot/common/properties/BooleanProperty.java
+++ src/xbot/common/properties/BooleanProperty.java
@@ -1,12 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package xbot.common.properties;
-
-import org.apache.log4j.Logger;
 
 /**
  * A property holding a boolean value.
