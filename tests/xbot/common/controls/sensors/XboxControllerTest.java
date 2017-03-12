ee1c54666431214ca86f9a51ecae2603b89b1f0e
John Gilbert <johngilb@gmail.com>
2017-03-12 12:36:14 -0700

    don't need the rule approach when expecting exceptions

--- tests/xbot/common/controls/sensors/XboxControllerTest.java
+++ tests/xbot/common/controls/sensors/XboxControllerTest.java
@@ -1,8 +1,6 @@
 package xbot.common.controls.sensors;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import xbot.common.controls.sensors.XXboxController.XboxButton;
 import xbot.common.injection.BaseWPITest;
@@ -11,16 +9,13 @@ import xbot.common.logging.RobotAssertionException;
 
 public class XboxControllerTest extends BaseWPITest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-    
-    @Test
+    @Test(expected = RobotAssertionException.class)
     public void doubleAllocateButton() {
         WPIFactory factory = injector.getInstance(WPIFactory.class);
         XXboxController controller = factory.getXboxController(0);
         
         // We expect the robot to get mad if we try to get the same button twice
-        thrown.expect(RobotAssertionException.class);
+        //thrown.expect(RobotAssertionException.class);
         controller.getXboxButton(XboxButton.A);
         controller.getXboxButton(XboxButton.A);
     }
