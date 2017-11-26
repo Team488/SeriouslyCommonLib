ec3828113656acdca5779b4e90ce479a6ac8082c
John Gilbert <johngilb@gmail.com>
2017-11-26 09:40:50 -0800

    Different idea for how the drive subsystem knows about talons

--- src/xbot/common/subsystems/BaseDriveSubsystem.java
+++ src/xbot/common/subsystems/BaseDriveSubsystem.java
@@ -1,19 +1,25 @@
 package xbot.common.subsystems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.DoubleStream;
 
+import com.ctre.CANTalon.TalonControlMode;
+
 import xbot.common.command.BaseSubsystem;
+import xbot.common.controls.actuators.XCANTalon;
 import xbot.common.math.XYPair;
 import xbot.common.properties.XPropertyManager;
 import xbot.common.subsystems.pose.BasePoseSubsystem;
 
 public abstract class BaseDriveSubsystem extends BaseSubsystem {
 
-    public BaseDriveSubsystem(BasePoseSubsystem pose, XPropertyManager propMan) {
-        
-    }
+    DrivePlatform platform;
     
+    public BaseDriveSubsystem(DrivePlatform platform) {
+        this.platform = platform;
+    }
+    /*
     public abstract void setFrontLeftMotor(double value);
     public abstract void setFrontRightMotor(double value);
     public abstract void setRearLeftMotor(double value);
@@ -27,21 +33,31 @@ public abstract class BaseDriveSubsystem extends BaseSubsystem {
     protected void setRightSide(double value) {
         setFrontRightMotor(value);
         setRearRightMotor(value);
+    }*/
+    
+    protected void setTalonModes(TalonControlMode mode) {
+        platform.getAllMasterTalons().stream()
+        .forEach((t) -> t.ensureTalonControlMode(mode));
     }
     
     public void simpleTankDrive(double leftPower, double rightPower) {
-        setLeftSide(leftPower);
-        setRightSide(rightPower);
+        setTalonModes(TalonControlMode.PercentVbus);
+        
+        platform.getLeftMasterTalon().set(leftPower);
+        platform.getRightMasterTalon().set(rightPower);
     }
     
     public void simpleHolonomicDrive(XYPair translation, double rotation, boolean normalizePower) {
+        setTalonModes(TalonControlMode.PercentVbus);
+        
         double fl_power = translation.y + translation.x - rotation;
         double fr_power = translation.y - translation.x + rotation;
         double rl_power = translation.y - translation.x - rotation;
         double rr_power = translation.y + translation.x + rotation;
         
         DoubleStream powers = Arrays.stream(new double[]{fl_power, fr_power, rl_power, rr_power});
-        double max = powers.map((e) -> Math.abs(e)).max().getAsDouble();
+        double max = powers.map((e) -> Math.abs(e))
+                           .max().getAsDouble();
         
         if (max > 1 && normalizePower) {
             fl_power /= max;
@@ -50,10 +66,10 @@ public abstract class BaseDriveSubsystem extends BaseSubsystem {
             rr_power /= max;
         }
         
-        setFrontLeftMotor(fl_power);
-        setFrontRightMotor(fr_power);
-        setRearLeftMotor(rl_power);
-        setRearRightMotor(rr_power);
+        platform.getFrontLeftMasterTalon().set(fl_power);
+        platform.getFrontRightMasterTalon().set(fr_power);
+        platform.getRearLeftMasterTalon().set(rl_power);
+        platform.getRearRightMasterTalon().set(rr_power);
     }
     
     public void simpleHolonomicDrive(XYPair translation, double rotation) {
