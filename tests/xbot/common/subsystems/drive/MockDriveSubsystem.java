9880062669e8c235fa34b2dc7f63b2da3456d5fd
John Gilbert <johngilb@gmail.com>
2017-12-09 12:25:04 -0800

    getting ready for tests

--- tests/xbot/common/subsystems/drive/MockDriveSubsystem.java
+++ tests/xbot/common/subsystems/drive/MockDriveSubsystem.java
@@ -1,5 +1,6 @@
 package xbot.common.subsystems.drive;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.google.inject.Inject;
@@ -7,19 +8,45 @@ import com.google.inject.Singleton;
 
 import xbot.common.subsystems.BaseDriveSubsystem;
 import xbot.common.controls.actuators.XCANTalon;
+import xbot.common.injection.wpi_factories.CommonLibFactory;
 
 @Singleton
 public class MockDriveSubsystem extends BaseDriveSubsystem {
 
+    Map<XCANTalon, MotionRegistration> masterTalons;
+    
     @Inject
-    public MockDriveSubsystem() {
-        super();
-        // TODO Auto-generated constructor stub
+    public MockDriveSubsystem(CommonLibFactory clf) {
+        masterTalons = new HashMap<XCANTalon, MotionRegistration>();
+        
+        // left talon
+        masterTalons.put(clf.createCANTalon(0), new MotionRegistration(0, 1, -1));
+        
+        // right talon
+        masterTalons.put(clf.createCANTalon(1), new MotionRegistration(0, 1, 1));
     }
 
     @Override
     protected Map<XCANTalon, MotionRegistration> getAllMasterTalons() {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public double getLeftTotalDistance() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getRightTotalDistance() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getTransverseDistance() {
+        // TODO Auto-generated method stub
+        return 0;
     }
 }
