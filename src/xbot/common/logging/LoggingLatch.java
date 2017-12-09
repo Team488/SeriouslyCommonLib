33026c82f41726c06859b58025fe89774e993759
imathno <imathno@hotmail.com>
2017-12-09 15:14:02 -0800

    LoggingLatch now uses Latch to check either Falling or Rising edge

--- src/xbot/common/logging/LoggingLatch.java
+++ src/xbot/common/logging/LoggingLatch.java
@@ -1,25 +1,32 @@
 package xbot.common.logging;
 
+import java.util.Observable;
+
 import org.apache.log4j.Logger;
+
+import xbot.common.logic.Latch;
+import xbot.common.logic.Latch.EdgeType;
 
 public class LoggingLatch {
 
     static Logger log = Logger.getLogger(LoggingLatch.class);
-    private String callerName;
-    private String message;
-    private boolean oldValue;
     
-    public LoggingLatch(String callerName, String message) {
-        this.callerName = callerName;
-        oldValue = false;
+    private final Latch latch;
+    
+    public LoggingLatch(String callerName, String message, EdgeType edgeType) {
+        this.latch = new Latch(false, edgeType);
+        
+        latch.addObserver((Observable o, Object arg) -> {
+            if(arg instanceof EdgeType) {
+                EdgeType edge = (EdgeType)arg;
+                if(edge == edgeType) {
+                    log.error(callerName + ": " + message);
+                }
+            }
+        });
     }
     
     public void checkValue(boolean value) {
-        if (value && !oldValue) {
-            // rising edge!
-            log.error("From " + callerName + ": " + message);
-        }
-        
-        oldValue = value;
+        latch.setValue(value);
     }
 }
