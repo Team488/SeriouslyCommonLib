db7b7175a7149cfdbb07825fd2c154979cfccbae
WasabiFan <wasabifan@outlook.com>
2018-01-19 23:09:37 -0800

    Add ability to run command while button is pressed, but not re-start it after it finishes

--- src/main/java/xbot/common/controls/sensors/AdvancedButton.java
+++ src/main/java/xbot/common/controls/sensors/AdvancedButton.java
@@ -1,6 +1,10 @@
 package xbot.common.controls.sensors;
 
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.Trigger.ButtonScheduler;
+import edu.wpi.first.wpilibj.command.Command;
+import xbot.common.logic.Latch;
+import xbot.common.logic.Latch.EdgeType;
 
 public abstract class AdvancedButton extends Button
 {
@@ -15,5 +19,30 @@ public abstract class AdvancedButton extends Button
     
     public boolean getInverted() {
         return isInverted;
+    }
+    
+    public void whilePressedSingleRun(final Command command) {
+    	// Based on source from WPILib's Trigger
+        new ButtonScheduler() {
+            private boolean pressedLast = get();
+            private Latch pressedLatch = new Latch(get(), EdgeType.Both, edge -> { 
+                if (edge == EdgeType.RisingEdge) {
+                    command.start();
+                }
+                else if (edge == EdgeType.FallingEdge) {
+                    command.cancel();
+                }
+            });
+          
+            @Override
+            public void start() {
+                super.start();
+            }
+
+            @Override
+            public void execute() {
+          	    pressedLatch.setValue(get());
+            }
+        }.start();
     }
 }
