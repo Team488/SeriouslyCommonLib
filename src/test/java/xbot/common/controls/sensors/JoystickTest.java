5586b60a39953724961aac48219e5b8df45de1e8
JohnGilb <johngilb@gmail.com>
2018-01-30 20:30:42 -0800

    Check the behavior of analog HID button (#128)

---
+++ src/test/java/xbot/common/controls/sensors/JoystickTest.java
@@ -0,0 +1,28 @@
+package xbot.common.controls.sensors;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import xbot.common.controls.sensors.AnalogHIDButton.AnalogHIDDescription;
+import xbot.common.controls.sensors.mock_adapters.MockJoystick;
+import xbot.common.injection.BaseWPITest;
+
+public class JoystickTest extends BaseWPITest {
+
+    MockJoystick joystick;
+    
+    @Override
+    public void setUp() {
+        super.setUp();
+        
+        joystick = (MockJoystick)clf.createJoystick(0, 10);
+    }
+    
+    @Test
+    public void testAnalogButton() {
+        AnalogHIDDescription desc = new AnalogHIDDescription(0, -1, -.1);
+        joystick.addAnalogButton(desc);
+        assertTrue(joystick.getAnalogIfAvailable(desc) != null);        
+    }
+}
