36cefd4898748ffcd65e5f8cdb888d83370b6474
John Gilbert <johngilb@gmail.com>
2018-02-08 20:29:36 -0800

    Need device police on digital IO

--- src/main/java/xbot/common/controls/sensors/XDigitalInput.java
+++ src/main/java/xbot/common/controls/sensors/XDigitalInput.java
@@ -1,8 +1,17 @@
 package xbot.common.controls.sensors;
 
+import com.google.inject.Inject;
+
 import xbot.common.controls.XBaseIO;
+import xbot.common.injection.wpi_factories.DevicePolice;
+import xbot.common.injection.wpi_factories.DevicePolice.DeviceType;
 
 public abstract class XDigitalInput implements XBaseIO {
 
+    @Inject
+    public XDigitalInput(DevicePolice police, int channel) {
+        police.registerDevice(DeviceType.DigitalIO, channel);
+    }
+    
     public abstract boolean get();
 }
