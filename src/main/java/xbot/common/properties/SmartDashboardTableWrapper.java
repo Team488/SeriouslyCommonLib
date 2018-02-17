9319e225effa6b28ae0817ad98f170aafb647b6d
John Gilbert <johngilb@gmail.com>
2018-02-17 09:28:20 -0800

    SmartDashboardTableWrapper now : - Always writes to an in-memory table - In "slow mode", also writes to the Smart Dashboard - In "slow mode", reads from the SD - In "fast mode", reads from the in-memory table

--- src/main/java/xbot/common/properties/SmartDashboardTableWrapper.java
+++ src/main/java/xbot/common/properties/SmartDashboardTableWrapper.java
@@ -6,18 +6,27 @@ package xbot.common.properties;
 
 import org.apache.log4j.Logger;
 
+import com.google.inject.Singleton;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  * @author Alex
  */
+@Singleton
 public class SmartDashboardTableWrapper implements ITableProxy {
 
     private static final Logger log = Logger.getLogger(SmartDashboardTableWrapper.class);
     boolean fastMode = false;
+    private TableProxy fastTable;
+
+    public SmartDashboardTableWrapper() {
+        fastTable = new TableProxy();
+    }
 
     public void setDouble(String key, double value) {
+        fastTable.setDouble(key, value);
         if (!fastMode) {
             SmartDashboard.putNumber(key, value);
         }
@@ -30,39 +39,50 @@ public class SmartDashboardTableWrapper implements ITableProxy {
     }
 
     public Double getDouble(String key) {
-        if (SmartDashboard.getKeys().contains(key)) {
-            return Double.valueOf(SmartDashboard.getNumber(key, 0));
-        } else  {
-            return null;
+        if (!fastMode) {
+            if (SmartDashboard.getKeys().contains(key)) {
+                return Double.valueOf(SmartDashboard.getNumber(key, 0));
+            } else {
+                return null;
+            }
         }
+        return fastTable.getDouble(key);
     }
 
     public void setBoolean(String key, boolean value) {
+        fastTable.setBoolean(key, value);
         if (!fastMode) {
             SmartDashboard.putBoolean(key, value);
         }
     }
 
     public Boolean getBoolean(String key) {
-        if (SmartDashboard.getKeys().contains(key)) {
-            return Boolean.valueOf(SmartDashboard.getBoolean(key, false));
-        } else  {
-            return null;
+        if (!fastMode) {
+            if (SmartDashboard.getKeys().contains(key)) {
+                return Boolean.valueOf(SmartDashboard.getBoolean(key, false));
+            } else {
+                return null;
+            }
         }
+        return fastTable.getBoolean(key);
     }
 
     public void setString(String key, String value) {
+        fastTable.setString(key, value);
         if (!fastMode) {
             SmartDashboard.putString(key, value);
         }
     }
 
     public String getString(String key) {
-        if (SmartDashboard.getKeys().contains(key)) {
-            return SmartDashboard.getString(key, "");
-        } else  {
-            return null;
+        if (!fastMode) {
+            if (SmartDashboard.getKeys().contains(key)) {
+                return SmartDashboard.getString(key, "");
+            } else {
+                return null;
+            }
         }
+        return fastTable.getString(key);
     }
 
     @Override
