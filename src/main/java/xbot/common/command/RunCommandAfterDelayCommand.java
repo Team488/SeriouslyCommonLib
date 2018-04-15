bf5e281310d2f69d3863eba1c2168f83abbd99a2
WasabiFan <wasabifan@outlook.com>
2018-04-15 13:20:08 -0700

    Add delayed command infrastructure

---
+++ src/main/java/xbot/common/command/RunCommandAfterDelayCommand.java
@@ -0,0 +1,15 @@
+package xbot.common.command;
+
+import com.google.inject.Provider;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class RunCommandAfterDelayCommand extends BaseCommandGroup {
+    public RunCommandAfterDelayCommand(Command command, double timeout, Provider<TimeoutCommand> timeoutProvider) {
+        TimeoutCommand timeoutCommand = timeoutProvider.get();
+        timeoutCommand.setConfigurableTimeout(timeout);
+        
+        this.addSequential(timeoutCommand);
+        this.addSequential(command);
+    }
+}
