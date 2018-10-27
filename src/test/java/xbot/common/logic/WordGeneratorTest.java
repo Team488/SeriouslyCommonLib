3ce740d0e32b9f8dccd7b2dd3dcb5a7469d1d68b
John Gilbert <johngilb@gmail.com>
2018-10-27 14:03:07 -0700

    Add resources and classes to generate word-based session IDs for the robot

---
+++ src/test/java/xbot/common/logic/WordGeneratorTest.java
@@ -0,0 +1,33 @@
+package xbot.common.logic;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import xbot.common.injection.BaseWPITest;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class WordGeneratorTest extends BaseWPITest {
+
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void testWords() {
+        WordGenerator wg = new WordGenerator();
+        String chain = wg.getRandomWordChain(3, "-");
+        System.out.println(chain);
+    }
+
+    @Test
+    public void testOneWord() {
+        WordGenerator wg = new WordGenerator();
+        String chain = wg.getRandomWordChain(1, "-");
+        assertFalse("Should only contain one word with no separator", chain.contains("-"));
+    }
+}
