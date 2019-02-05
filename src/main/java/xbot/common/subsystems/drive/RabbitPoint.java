38e9a0f66ac84c5974ddb1dabcfef03b5095b879
John Gilbert <johngilb@gmail.com>
2019-02-04 18:42:15 -0800

    cleanup

--- src/main/java/xbot/common/subsystems/drive/RabbitPoint.java
+++ src/main/java/xbot/common/subsystems/drive/RabbitPoint.java
@@ -10,30 +10,26 @@ import xbot.common.math.XYPair;
 public class RabbitPoint {
 
     public enum PointType {
-        PositionAndHeading,
-        HeadingOnly,
-        PositionOnly
+        PositionAndHeading, HeadingOnly, PositionOnly
     }
-    
+
     public enum PointTerminatingType {
-        Continue,
-        Stop
+        Continue, Stop
     }
-    
+
     public enum PointDriveStyle {
-        Macro,
-        Micro
+        Macro, Micro
     }
-    
+
     public PointType pointType;
     public PointTerminatingType terminatingType;
     public PointDriveStyle driveStyle;
     public FieldPose pose;
-    
+
     public RabbitPoint(double x, double y, double heading) {
         this(new FieldPose(new XYPair(x, y), new ContiguousHeading(heading)));
     }
-    
+
     public RabbitPoint(FieldPose pose) {
         this(pose, PointType.PositionAndHeading, PointTerminatingType.Continue, PointDriveStyle.Macro);
     }
@@ -42,19 +38,20 @@ public class RabbitPoint {
         this(pose, pointType, terminatingType, PointDriveStyle.Macro);
     }
 
-	public RabbitPoint(FieldPose pose, PointType pointType, PointTerminatingType terminatingType, PointDriveStyle driveStyle) {
+    public RabbitPoint(FieldPose pose, PointType pointType, PointTerminatingType terminatingType,
+            PointDriveStyle driveStyle) {
         this.pose = pose;
         this.pointType = pointType;
         this.terminatingType = terminatingType;
         this.driveStyle = driveStyle;
     }
-    
+
     public static List<RabbitPoint> upgradeFieldPoseList(List<FieldPose> oldPoints) {
         List<RabbitPoint> rabbitPoints = new ArrayList<>();
         oldPoints.stream().forEach(point -> rabbitPoints.add(new RabbitPoint(point)));
         return rabbitPoints;
     }
-    
+
     public static List<FieldPose> downgradeRabbitPointList(List<RabbitPoint> rabbitPoints) {
         List<FieldPose> fieldPoints = new ArrayList<>();
         rabbitPoints.stream().forEach(rabbitPoint -> fieldPoints.add(rabbitPoint.pose));
