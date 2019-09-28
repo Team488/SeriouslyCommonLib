08ab5da914052644d46a0bf98c7882fcf8323730
Stephen Just <stephenjust@gmail.com>
2019-09-28 15:52:36 -0700

    XYPair improvements (#221)
    * XYPair improvements
    
    * Add javadoc to methods
    * Unit tests for methods
    * Modify addMagnitude() to behave consistently
       with the other methods in this class (i.e. modify existing object)
    
    * Boost code coverage, fix addMagnitude case where initial magnitude == 0

--- src/main/java/xbot/common/math/XYPair.java
+++ src/main/java/xbot/common/math/XYPair.java
@@ -3,7 +3,6 @@ package xbot.common.math;
 /**
  * Pair of X and Y coordinates. Can be used for points, vectors, or anything
  * else that requires a coordinate pair.
- *
  */
 public class XYPair {
     public static final XYPair ZERO = new XYPair(0, 0);
@@ -11,58 +10,95 @@ public class XYPair {
     public double x;
     public double y;
 
+    /**
+     * Create a coordinate with the specified x and y value.
+     * @param x the x value to use
+     * @param y the y value to use
+     */
     public XYPair(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Create a coordinate at the origin.
+     */
     public XYPair() {
         this.x = 0;
         this.y = 0;
     }
 
+    /**
+     * Clone the object
+     */
     public XYPair clone() {
         return new XYPair(x, y);
     }
 
+    /**
+     * Create a coordinate from an angle and magnitude
+     * @param angle angle in degrees
+     * @param magnitude magnitude
+     * @return coordinate
+     */
     public static XYPair fromPolar(double angle, double magnitude) {
         XYPair newValue = new XYPair(magnitude, 0);
         return newValue.rotate(angle);
     }
     
+    /**
+     * Create a coordinate of magnitude 1 with an angle
+     * @param angle angle in degrees
+     * @return coordinate
+     */
     public static XYPair fromUnitPolar(double angle) {
         XYPair newValue = new XYPair(1, 0);
         return newValue.rotate(angle);
     }
 
+    /**
+     * Scales the current coordinate by a magnitude equally in both dimensions.
+     * @param scalarMagnitude the magnitude to scale the coordinate by
+     * @return the scaled object
+     */
     public XYPair scale(double scalarMagnitude) {
         this.x *= scalarMagnitude;
         this.y *= scalarMagnitude;
         return this;
     }
 
+    /**
+     * Scales the coordinate by an x- and y- magnitude.
+     * @param xMagnitude the magnitude to scale the x-coordinate by
+     * @param yMagnitude the magnitude to scale the y-coordinate by
+     * @return the scaled object
+     */
     public XYPair scale(double xMagnitude, double yMagnitude) {
         this.x *= xMagnitude;
         this.y *= yMagnitude;
         return this;
     }
     
+    /**
+     * Add the magnitude to the vector represented by the coordinate.
+     * @param magnitudeToAdd magnitude to add
+     * @return the modified coordinate
+     */
     public XYPair addMagnitude(double magnitudeToAdd) {
         double currentMagnitude = this.getMagnitude();
         
         if (currentMagnitude == 0) {
-            return new XYPair(0, magnitudeToAdd);
+            this.y = magnitudeToAdd;
+            return this;
         }
         
         double totalMagnitude = currentMagnitude + magnitudeToAdd;
-        return this.clone().scale(totalMagnitude/ currentMagnitude);
+        return this.scale(totalMagnitude / currentMagnitude);
     }
 
     /**
-     * Rotates the current coordinates by a given angle.
-     * 
-     * @param angle
-     *            The angle (in degrees) to rotate the pair by
+     * Rotates the vector by a given angle.
+     * @param angle the angle in degrees to rotate the pair by
      * @return the rotated object
      */
     public XYPair rotate(double angle) {
@@ -71,26 +107,44 @@ public class XYPair {
         double tempX = x * cosA - y * sinA;
         double tempY = x * sinA + y * cosA;
 
-        x = tempX;
-        y = tempY;
+        this.x = tempX;
+        this.y = tempY;
 
         return this;
     }
 
+    /**
+     * Get the angle of the vector in degrees
+     * @return angle in degrees
+     */
     public double getAngle() {
         return Math.toDegrees(Math.atan2(y, x));
     }
 
+    /**
+     * Get the magnitude of the vector (i.e. distance from origin to the coordinate).
+     * @return magnitude
+     */
     public double getMagnitude() {
         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
+    /**
+     * Adds the value of a second coordinate to the current coordinate.
+     * @param pair coordinate to add
+     * @return the combined object
+     */
     public XYPair add(XYPair pair) {
         this.x += pair.x;
         this.y += pair.y;
         return this;
     }
     
+    /**
+     * Get the distance between the current point and a second point.
+     * @param otherPoint the point to calculate the distance from
+     * @return the distance
+     */
     public double getDistanceToPoint(XYPair otherPoint) {
         // Subtract the points from each other
         XYPair normalizedPoint = this.clone().add(otherPoint.clone().scale(-1));
@@ -98,6 +152,11 @@ public class XYPair {
         return normalizedPoint.getMagnitude();
     }
 
+    /**
+     * Calculate the dot-product between this and another point.
+     * @param otherPoint the second point to use for calculating dot product
+     * @return the dot product value
+     */
     public double dotProduct(XYPair otherPoint) {
         return (this.x * otherPoint.x) + (this.y * otherPoint.y);
     }
