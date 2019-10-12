68cad26b68abbe7627cb349c09af985951745de9
alyssaki <kiehnalyssa@gmail.com>
2019-10-12 11:55:29 -0700

    more style changes

--- src/test/java/xbot/common/controls/actuators/XDoubleSolenoidTest.java
+++ src/test/java/xbot/common/controls/actuators/XDoubleSolenoidTest.java
@@ -1,6 +1,7 @@
 package xbot.common.controls.actuators;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Before;
 import org.junit.Test;
