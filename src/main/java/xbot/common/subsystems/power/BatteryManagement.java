059842919dc15cbe07812bedc53b838084f0c1c9
alyssaki <kiehnalyssa@gmail.com>
2019-11-09 13:47:26 -0800

    added a class to display how much power has been used

--- src/main/java/xbot/common/subsystems/power/BatteryManagement.java
+++ src/main/java/xbot/common/subsystems/power/BatteryManagement.java
@@ -6,30 +6,63 @@ import xbot.common.controls.sensors.XAnalogInput;
 import xbot.common.controls.sensors.XGyro;
 import xbot.common.controls.sensors.XPowerDistributionPanel;
 import xbot.common.controls.sensors.XTimer;
+import xbot.common.subsystems.drive.BaseDriveSubsystem;
 
 public class BatteryManagement extends BaseSubsystem implements PeriodicDataSource {
 
     double lastPowerRecorded;
     double periodicData;
+    double totalPowerUsed;
     XPowerDistributionPanel panel;
     XAnalogInput analogInput;
+    BaseDriveSubsystem baseDrive;
 
     @Inject
-    public BatteryManagement(XPowerDistributionPanel panel, XAnalogInput analogInput)
+    public BatteryManagement(XPowerDistributionPanel panel, XAnalogInput analogInput, BaseDriveSubsystem baseDrive)
     {
         this.panel = panel;
         this.analogInput = analogInput;
+        lastPowerRecorded = 0;
+        totalPowerUsed = 0;
+        this.baseDrive = baseDrive;
     }
 
+    //update the total power and current power
     public void updatePeriodicData()
     {
+        //equation for power is Current * voltage
+        //getTotalCurrent gets the current from all the input panels
         double power = panel.getTotalCurrent() * analogInput.getVoltage();
-        periodicData = lastPowerRecorded - power;
+        periodicData = power;
+        totalPowerUsed += power;
+        //periodicData = Abs(lastPowerRecorded - power);
     }
 
-    public String getName()
+/*
+    public void antiChattering(double powerLimit, int secondsFromNeutralToFullWhenChattering, int regularSecondsFromNeutralToFull)
     {
-        return "";
+        if (totalPowerUsed > powerLimit)
+        {
+            baseDrive.setVoltageRamp(secondsFromNeutralToFullWhenChattering);
+            //setCurrentLimits()
+            //configVoltageCompSaturation
+        }
+        else
+        {
+            baseDrive.setVoltageRamp(regularSecondsFromNeutralToFull);
+        }
+        //setVoltageRamp() --> BaseDriveSubsystem.java
+        //setCurrentLimits()
     }
+
+    /*
+    public void antiChattering(double batteryVoltageLimit)
+    {
+        if (analogInput.getVoltage() > batteryVoltageLimit)
+        {
+            baseDrive.setVoltageRamp(secondsFromNeutralToFullWhenChattering);
+        }
+    }
+    */
 
 }
