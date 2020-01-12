f95aa6bd2ead479916ebff86aa6344b684e477ba
John Gilbert <johngilb@gmail.com>
2020-01-12 13:53:55 -0800

    RAMSETE almost working

--- src/test/java/xbot/common/command/SetpointSystemTest.java
+++ src/test/java/xbot/common/command/SetpointSystemTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import xbot.common.injection.BaseWPITest;
 
