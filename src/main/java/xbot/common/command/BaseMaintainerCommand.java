a162cba1a1522bd04562f0222b8e29d713fa8805
JohnGilb <johngilb@gmail.com>
2020-02-17 14:39:50 -0800

    We have maintainer subsystem, and setpoint commands - complete the tr… (#252)
    * We have maintainer subsystem, and setpoint commands - complete the trifecta with the BaseMaintainerCommand
    
    * appease checkstyle

---
+++ src/main/java/xbot/common/command/BaseMaintainerCommand.java
@@ -0,0 +1,25 @@
+package xbot.common.command;
+
+public abstract class BaseMaintainerCommand extends BaseCommand {
+
+    BaseSetpointSubsystem subsystemToMaintan;
+
+    public BaseMaintainerCommand(BaseSetpointSubsystem subsystemToMaintain) {
+        this.subsystemToMaintan = subsystemToMaintain;
+        this.addRequirements(subsystemToMaintain);
+    }
+
+    @Override
+    public void execute() {
+        maintain();
+        subsystemToMaintan.setMaintainerIsAtGoal(isMaintainerAtGoal());
+    }
+
+    /**
+     * Contains all the logic associated with keeping the subsystem
+     * at its goal.
+     */
+    protected abstract void maintain();
+
+    protected abstract boolean isMaintainerAtGoal();
+}
