90f2966b5b2132410e50c5c0bf7642406e6a573d
John Gilbert <johngilb@gmail.com>
2021-04-08 18:26:41 -0700

    Appease checkstyle

--- src/main/java/xbot/common/logging/IntermittentConsoleLogger.java
+++ src/main/java/xbot/common/logging/IntermittentConsoleLogger.java
@@ -5,7 +5,7 @@ public class IntermittentConsoleLogger {
     private int i = 0;
     private int modulus = 500;
 
-    public void LogSometimes(String message) {
+    public void logSometimes(String message) {
         if (i % modulus == 0) {
             System.out.println(message);
         }
