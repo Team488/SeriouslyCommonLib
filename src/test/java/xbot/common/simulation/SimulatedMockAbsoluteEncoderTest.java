f0114e97eebd4b74c1a83a84b7af1480fe0222e6
Stephen Just <stephenjust@gmail.com>
2022-02-03 20:23:27 -0800

    Add simulation hooks for MockAbsoluteEncoder (#311)
    * Add simulation hooks for MockAbsoluteEncoder
    
    * Use the same property name as other encoders
    
    * Add a test case
    
    * Checkstyle
    
    * Use hardware inversion capability

---
+++ src/test/java/xbot/common/simulation/SimulatedMockAbsoluteEncoderTest.java
@@ -0,0 +1,41 @@
+package xbot.common.simulation;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import xbot.common.controls.sensors.mock_adapters.MockAbsoluteEncoder;
+import xbot.common.injection.electrical_contract.DeviceInfo;
+
+public class SimulatedMockAbsoluteEncoderTest extends BaseSimulationTest {
+
+    MockAbsoluteEncoder simulatedEncoder;
+
+    @Override
+    public void setUp() {
+        super.setUp();
+
+        simulatedEncoder = (MockAbsoluteEncoder)clf.createAbsoluteEncoder(new DeviceInfo(34, false, 360.0), "test");
+    }
+
+    @Test
+    public void basicTest() {
+        JSONObject overallPayload = new JSONObject();
+        JSONObject singleSensor = new JSONObject();
+        singleSensor.put("ID", "CAN34");
+        JSONObject singleSensorPayload = new JSONObject();
+        singleSensorPayload.put("EncoderTicks", new BigDecimal("1.12"));
+        singleSensor.put("Payload", singleSensorPayload);
+        JSONArray sensorList = new JSONArray();
+        sensorList.put(singleSensor);
+        overallPayload.put("Sensors", sensorList);
+        
+        distributor.distributeSimulationPayload(overallPayload);
+
+        assertEquals(43.2, this.simulatedEncoder.getAbsolutePosition(), 0.001);
+    }
+}
