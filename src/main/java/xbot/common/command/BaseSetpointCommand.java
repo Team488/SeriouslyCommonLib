84900f06cd12ad1fee4098ce134802daafbf8c6b
John Gilbert <johngilb@gmail.com>
2022-02-11 17:43:31 -0800

    Allows for a setpoint command to manage multiple subsystems

--- src/main/java/xbot/common/command/BaseSetpointCommand.java
+++ src/main/java/xbot/common/command/BaseSetpointCommand.java
@@ -2,8 +2,10 @@ package xbot.common.command;
 
 public abstract class BaseSetpointCommand extends BaseCommand {
 
-    public BaseSetpointCommand(SupportsSetpointLock system) {
-        this.addRequirements(system.getSetpointLock());
+    public BaseSetpointCommand(SupportsSetpointLock... systems) {
+        for (SupportsSetpointLock system : systems) {
+            this.addRequirements(system.getSetpointLock());
+        }
     }
 
     @Override
