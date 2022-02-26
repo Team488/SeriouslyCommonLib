d66eea46417ac1e934eba070c9030e2e09633fbc
Stephen Just <stephenjust@gmail.com>
2022-02-26 11:41:15 -0800

    Feature/Add concept of a virtual button

---
+++ src/main/java/xbot/common/controls/sensors/VirtualButton.java
@@ -0,0 +1,32 @@
+package xbot.common.controls.sensors;
+
+/**
+ * A virtual button under program control that can be used for creating chords.
+ * This is useful to repurpose buttons in multiple "modes".
+ */
+public class VirtualButton extends AdvancedButton {
+
+    private boolean pressed;
+
+    public VirtualButton() {
+        this(true);
+    }
+
+    public VirtualButton(boolean defaultPressed) {
+        this.pressed = defaultPressed;
+    }
+    
+    @Override
+    public boolean getAsBoolean() {
+        return this.pressed;
+    }
+
+    public void set(boolean pressed) {
+        this.pressed = pressed;
+    }
+
+    public void toggle() {
+        this.pressed = !this.pressed;
+    }
+
+}
