ed37ba3d1d12ac8af61e9d8b38c3a54c0e78a8fd
Alex Schokking <aschokking@gmail.com>
2022-03-12 16:13:08 -0800

    Plumb through compressor enable/disable

--- src/main/java/edu/wpi/first/wpilibj/MockCompressor.java
+++ src/main/java/edu/wpi/first/wpilibj/MockCompressor.java
@@ -6,8 +6,25 @@ import xbot.common.controls.actuators.XCompressor;
 
 public class MockCompressor extends XCompressor {
     
+    private boolean isEnabled = true;
+
     @Inject
     public MockCompressor() {
 
+    }
+
+    @Override
+    public void disable() {
+        isEnabled = false;
+    }
+
+    @Override
+    public void enable() {
+        isEnabled = true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
     }
 }
