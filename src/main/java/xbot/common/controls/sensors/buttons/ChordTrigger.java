c34d9f7ace8f7b29c646e3e76ff2ff412abfb4c7
Stephen Just <stephenjust@gmail.com>
2023-03-14 21:12:28 -0700

    Use Trigger instead of AdvancedTrigger in ChordTrigger (#381)

--- src/main/java/xbot/common/controls/sensors/buttons/ChordTrigger.java
+++ src/main/java/xbot/common/controls/sensors/buttons/ChordTrigger.java
@@ -3,6 +3,7 @@ package xbot.common.controls.sensors.buttons;
 import dagger.assisted.Assisted;
 import dagger.assisted.AssistedFactory;
 import dagger.assisted.AssistedInject;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class ChordTrigger extends AdvancedTrigger {
 
@@ -12,12 +13,12 @@ public class ChordTrigger extends AdvancedTrigger {
     @AssistedFactory
     public abstract static class ChordTriggerFactory {
         public abstract ChordTrigger create(
-            @Assisted("a") AdvancedTrigger a,
-            @Assisted("b") AdvancedTrigger b);
+            @Assisted("a") Trigger a,
+            @Assisted("b") Trigger b);
     }
 
     @AssistedInject
-    public ChordTrigger(@Assisted("a") AdvancedTrigger a, @Assisted("b") AdvancedTrigger b) {
+    public ChordTrigger(@Assisted("a") Trigger a, @Assisted("b") Trigger b) {
         super(() -> a.getAsBoolean() && b.getAsBoolean());
     }
     
