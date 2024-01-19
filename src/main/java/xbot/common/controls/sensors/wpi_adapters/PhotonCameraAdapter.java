4cda7186a646a426432ea61da7b2a851c722122f
JohnGilb <johngilb@gmail.com>
2024-01-18 19:23:09 -0800

    Stephen's suggestion - much simpler way

--- src/main/java/xbot/common/controls/sensors/wpi_adapters/PhotonCameraAdapter.java
+++ src/main/java/xbot/common/controls/sensors/wpi_adapters/PhotonCameraAdapter.java
@@ -3,24 +3,13 @@ package xbot.common.controls.sensors.wpi_adapters;
 import dagger.assisted.Assisted;
 import dagger.assisted.AssistedFactory;
 import dagger.assisted.AssistedInject;
-import edu.wpi.first.math.MatBuilder;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.Nat;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N2;
-import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.numbers.N5;
-import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.PhotonCameraExtended;
 import xbot.common.controls.io_inputs.XPhotonCameraInputs;
 import xbot.common.controls.sensors.XPhotonCamera;
-import xbot.common.vision.XbotPhotonCameraFork;
-
-import java.util.Optional;
 
 public class PhotonCameraAdapter extends XPhotonCamera {
 
-    XbotPhotonCameraFork camera;
+    PhotonCameraExtended camera;
 
     @AssistedFactory
     public abstract static class PhotonCameraAdapterFactory implements XPhotonCameraFactory {
@@ -30,7 +19,7 @@ public class PhotonCameraAdapter extends XPhotonCamera {
     @AssistedInject
     public PhotonCameraAdapter(@Assisted String cameraName) {
         super(cameraName);
-        camera = new XbotPhotonCameraFork(cameraName);
+        camera = new PhotonCameraExtended(cameraName);
     }
 
     @Override
