db2959da1d5e265669ed6cdd508c962d45d6a47b
John Gilbert <johngilb@gmail.com>
2024-01-24 11:00:55 -0800

    Add new version checking that won't crash the robot

--- src/main/java/org/photonvision/PhotonCameraExtended.java
+++ src/main/java/org/photonvision/PhotonCameraExtended.java
@@ -1,16 +1,21 @@
 package org.photonvision;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.photonvision.targeting.PhotonPipelineResult;
 import xbot.common.controls.io_inputs.PhotonCameraExtendedInputsAutoLogged;
 
 public class PhotonCameraExtended extends PhotonCamera {
 
     PhotonCameraExtendedInputsAutoLogged io;
+    Logger log = LogManager.getLogger(this.getClass());
 
     public PhotonCameraExtended(NetworkTableInstance instance, String cameraName) {
         super(instance, cameraName);
         io = new PhotonCameraExtendedInputsAutoLogged();
+
     }
 
     public PhotonCameraExtended(String cameraName) {
@@ -26,6 +31,18 @@ public class PhotonCameraExtended extends PhotonCamera {
 
     public double[] getDistCoeffsRaw() {
         return io.distCoeffs;
+    }
+
+    public boolean doesLibraryVersionMatchCoprocessorVersion() {
+        // Check for version. Warn if the versions aren't aligned.
+        String versionString = versionEntry.get("");
+        if (!versionString.isEmpty() && !PhotonVersion.versionMatches(versionString)) {
+            // Error on a verified version mismatch
+            // But stay silent otherwise
+            log.error("PhotonVision version mismatch. Expected: " + PhotonVersion.versionString + " Actual: " + versionString + ". Unexpected behavior may occur.");
+            return false;
+        }
+        return true;
     }
 
     public void refreshDataFrame() {
