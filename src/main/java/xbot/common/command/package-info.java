3d739033132f09f37cf3b071ff832aa1d81b4ba6
Stephen Just <stephenjust@gmail.com>
2024-01-27 10:01:55 -0800

    Address review comments

--- src/main/java/xbot/common/command/package-info.java
+++ src/main/java/xbot/common/command/package-info.java
@@ -6,18 +6,18 @@
  * There are many components of the robot that need to maintain a particular state.
  * For example, an arm might need to maintain a particular angle, or a drive train
  * might need to maintain a particular velocity. We can implement these components
- * as <em>setpoint subsystems</em> that have a <em>maintainer</em> that runs in the
+ * as <em>setpoint subsystems</em> that have a <em>maintainer command</em> that runs in the
  * background and keeps the subsystem and its outputs at the desired state.
  * </p>
  * <p>
  * Setpoint subsystems are implemented as {@link xbot.common.command.BaseSetpointSubsystem}s.
- * They have a {@link xbot.common.command.BaseMaintainerCommand} that runs on the subsystem.
- * {@link xbot.common.command.BaseSetpointCommand}s are used to change the setpoint of the
- * subsystem.
+ * They have a <em>command</em> that extends {@link xbot.common.command.BaseMaintainerCommand} that runs on
+ * the subsystem. <em>Commands</em> extending from {@link xbot.common.command.BaseSetpointCommand}s are used to
+ * change the setpoint of the subsystem.
  * </p>
  * <p>
  * A {@link xbot.common.command.BaseSetpointSubsystem} often has a default command that runs on it. This command
- * is a {@link xbot.common.command.BaseSetpointCommand} that returns the related mechanism
+ * is a {@link xbot.common.command.BaseSetpointCommand}-derived command that returns the related mechanism
  * to its default state. For example, an arm might have a default command that
  * returns the arm to its lowest position. Additional commands can be created to
  * move the arm to other positions. These commands will interrupt each other because
@@ -32,7 +32,7 @@
  * </p>
  * <pre class="mermaid">
      classDiagram
-         class MySetpointSubsystem {
+         class ShooterWheelSetpointSubsystem {
              -Device output
              -Sensor input
              +getCurrentValue() T
@@ -41,7 +41,15 @@
              +setPower(T power) void
              +isCalibrated() boolean
          }
-         class MyMaintainerCommand {
+         class BaseSetpointSubsystem~T~ {
+             +getCurrentValue()*T
+             +getTargetValue()* T
+             +setTargetValue(T value)* void
+             +setPower(T power)* void
+             +isCalibrated()* boolean
+         }
+         BaseSetpointSubsystem~T~ --|&gt; ShooterWheelSetpointSubsystem
+         class ShooterWheelMaintainerCommand {
              ~BaseSetpointSubsystem~T~ subsystemToMaintain
              +execute() void
              #coastAction() void
@@ -53,14 +61,34 @@
              #getHumanInput() T
              #getHumanInputMagnitude() double
          }
-         class MySetpointCommand {
+         class BaseMaintainerCommand~T~ {
+             ~BaseSetpointSubsystem~T~ subsystemToMaintain
+             +execute() void
+             #coastAction()* void
+             #humanControlAction()* void
+             #initializeMachineControlAction()* void
+             #calibratedMachineControlAction()* void
+             #uncalibratedMachineControlAction()* void
+             #getErrorMagnitude()* double
+             #getHumanInput()* T
+             #getHumanInputMagnitude()* double
+         }
+         BaseMaintainerCommand~T~ --|&gt; ShooterWheelMaintainerCommand
+         class StopShooterWheelSetpointCommand {
              -BaseSetpointSubsystem~T~ subsystem
              +initialize() void
              +execute() void
              +isFinished() boolean
          }
-         MyMaintainerCommand ..&gt; MySetpointSubsystem : Maintains outputs
-         MySetpointCommand ..&gt; MySetpointSubsystem : Sets targets
+         class BaseSetpointCommand~T~ {
+             -BaseSetpointSubsystem~T~ subsystem
+             +initialize()* void
+             +execute()* void
+             +isFinished()* boolean
+         }
+         BaseSetpointCommand~T~ --|&gt; StopShooterWheelSetpointCommand
+         ShooterWheelMaintainerCommand ..&gt; ShooterWheelSetpointSubsystem : Maintains outputs
+         StopShooterWheelSetpointCommand ..&gt; ShooterWheelSetpointSubsystem : Sets targets
  * </pre>
  */
 package xbot.common.command;
