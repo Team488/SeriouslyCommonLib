0576252d19f70fa25dead6a0de9de63846c2b99d
JohnGilb <johngilb@gmail.com>
2024-01-30 20:28:13 -0800

    Add moving average

---
+++ src/main/java/xbot/common/math/MovingAverage.java
@@ -0,0 +1,43 @@
+package xbot.common.math;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MovingAverage<T> {
+    private int SIZE = 15;
+    private Queue<T> queue;
+    private T sum;
+    private SumFunction<T> sumFunction;
+
+    public MovingAverage(SumFunction<T> sumFunction, T initialValue, int size) {
+        this.queue = new LinkedList<>();
+        this.sum = initialValue;
+        this.sumFunction = sumFunction;
+        this.SIZE = size;
+    }
+
+    public void add(T value) {
+        sum = sumFunction.add(sum, value);
+        queue.add(value);
+        if (queue.size() > SIZE) {
+            sum = sumFunction.subtract(sum, queue.remove());
+        }
+    }
+
+    public T getAverage() {
+        if (queue.isEmpty()) {
+            return sum;
+        }
+        return sumFunction.divide(sum, queue.size());
+    }
+
+    public interface SumFunction<T> {
+        T add(T a, T b);
+        T subtract(T a, T b);
+        T divide(T a, int b);
+    }
+
+}
+
