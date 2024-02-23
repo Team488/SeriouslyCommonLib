c90f33f5fa46ce69faf14fb2d9b92488dcf7a651
John Gilbert <johngilb@gmail.com>
2024-02-22 22:08:46 -0800

    DigitalInput supports inversion

---
+++ src/test/java/xbot/common/controls/sensors/DigitalInputTest.java
@@ -0,0 +1,23 @@
+package xbot.common.controls.sensors;
+
+import org.junit.Test;
+import xbot.common.injection.BaseCommonLibTest;
+import xbot.common.injection.electrical_contract.DeviceInfo;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DigitalInputTest extends BaseCommonLibTest {
+    @Test
+    public void testInversion() {
+        DeviceInfo info = new DeviceInfo("TestInput", 0, false, 0);
+        XDigitalInput digitalInput = getInjectorComponent().digitalInputFactory().create(info, "testPrefix");
+
+        assertFalse(digitalInput.getInverted());
+
+        info = new DeviceInfo("TestInput", 1, true, 0);
+        digitalInput = getInjectorComponent().digitalInputFactory().create(info, "testPrefix");
+
+        assertTrue(digitalInput.getInverted());
+    }
+}
