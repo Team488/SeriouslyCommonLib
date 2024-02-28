979e6d3690d3140a7086d7ae8655dba58d561644
Stephen Just <stephenjust@gmail.com>
2024-02-27 19:13:48 -0800

    [Vision] Re-genericise vision code and support multiple camera types (#458)
    * [Vision] Re-genericise vision code and support multiple camera types
    
    * Add javadoc

---
+++ src/main/java/xbot/common/subsystems/vision/AprilTagCamera.java
@@ -0,0 +1,53 @@
+package xbot.common.subsystems.vision;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import org.photonvision.PhotonPoseEstimator;
+import xbot.common.injection.electrical_contract.CameraInfo;
+import xbot.common.logic.TimeStableValidator;
+
+import java.util.function.Supplier;
+
+/**
+ * This class provides common base implementation for April Tag capable cameras on the robot.
+ */
+public class AprilTagCamera extends SimpleCamera {
+    private final PhotonPoseEstimator poseEstimator;
+
+    private final TimeStableValidator isStable;
+
+    /**
+     * Create a new AprilTagCamera.
+     *
+     * @param cameraInfo The information about the camera.
+     * @param poseStableTime The time that the pose must be stable for before it is considered valid.
+     * @param fieldLayout The layout of the field.
+     */
+    public AprilTagCamera(CameraInfo cameraInfo,
+                          Supplier<Double> poseStableTime,
+                          AprilTagFieldLayout fieldLayout) {
+        super(cameraInfo);
+        this.poseEstimator = new PhotonPoseEstimator(fieldLayout,
+                PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+                this.camera,
+                cameraInfo.position());
+        this.isStable = new TimeStableValidator(poseStableTime);
+    }
+
+    /**
+     * Get the pose estimator.
+     *
+     * @return The pose estimator.
+     */
+    public PhotonPoseEstimator getPoseEstimator() {
+        return this.poseEstimator;
+    }
+
+    /**
+     * Get the time stable validator.
+     *
+     * @return The time stable validator.
+     */
+    public TimeStableValidator getIsStableValidator() {
+        return isStable;
+    }
+}
---
+++ src/main/java/xbot/common/subsystems/vision/CameraCapabilities.java
@@ -0,0 +1,15 @@
+package xbot.common.subsystems.vision;
+
+/**
+ * This enum is used to specify the capabilities of a camera.
+ */
+public enum CameraCapabilities {
+    /**
+     * The camera is capable of providing April Tag localization information.
+     */
+    APRIL_TAG,
+    /**
+     * The camera is capable of providing game-specific information.
+     */
+    GAME_SPECIFIC
+}
