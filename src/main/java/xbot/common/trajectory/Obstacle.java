579b8e42a7f2d8ea95f7223388125dea2cb896cb
John Gilbert <johngilb@gmail.com>
2024-02-28 22:09:52 -0800

    1.5 meters was too much, this may be too little. Maybe somewhere in between, or more configurable?

--- src/main/java/xbot/common/trajectory/Obstacle.java
+++ src/main/java/xbot/common/trajectory/Obstacle.java
@@ -273,7 +273,7 @@ public class Obstacle extends Rectangle2D.Double {
         double xDelta = 0;
         double yDelta = 0;
         double minDistance = 10000;
-        double bonusOffset = 1.5;
+        double bonusOffset = 0.1;
 
         double topDistance = topLine.ptLineDist(point.getX(), point.getY());
         if (topDistance < minDistance && (topLeftAvailable || topRightAvailable)) {
