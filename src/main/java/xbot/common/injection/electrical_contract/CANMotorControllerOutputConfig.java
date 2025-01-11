4fec1500dab675e03aa0bd5a27ef7095a961ea6d
Stephen Just <stephenjust@gmail.com>
2025-01-11 14:47:23 -0800

    Add helpers to easily set motor configs

--- src/main/java/xbot/common/injection/electrical_contract/CANMotorControllerOutputConfig.java
+++ src/main/java/xbot/common/injection/electrical_contract/CANMotorControllerOutputConfig.java
@@ -2,6 +2,8 @@ package xbot.common.injection.electrical_contract;
 
 import edu.wpi.first.units.measure.Current;
 
+import static edu.wpi.first.units.Units.Amps;
+
 public class CANMotorControllerOutputConfig {
     public enum InversionType {
         Normal,
@@ -13,9 +15,24 @@ public class CANMotorControllerOutputConfig {
         Coast
     }
 
-    public InversionType inversionType;
+    public InversionType inversionType = InversionType.Normal;
 
-    public NeutralMode neutralMode;
+    public NeutralMode neutralMode = NeutralMode.Coast;
 
-    public Current statorCurrentLimit;
+    public Current statorCurrentLimit = Amps.of(80);
+
+    public CANMotorControllerOutputConfig withInversionType(InversionType inversionType) {
+        this.inversionType = inversionType;
+        return this;
+    }
+
+    public CANMotorControllerOutputConfig withNeutralMode(NeutralMode neutralMode) {
+        this.neutralMode = neutralMode;
+        return this;
+    }
+
+    public CANMotorControllerOutputConfig withStatorCurrentLimit(Current statorCurrentLimit) {
+        this.statorCurrentLimit = statorCurrentLimit;
+        return this;
+    }
 }
