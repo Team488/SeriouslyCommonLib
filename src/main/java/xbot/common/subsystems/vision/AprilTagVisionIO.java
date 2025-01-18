fc9f832cad1d4f49523d332c75837592e9110299
Stephen Just <stephenjust@gmail.com>
2025-01-18 12:02:02 -0800

    Expose more details about the targeted apriltag for consumption

--- src/main/java/xbot/common/subsystems/vision/AprilTagVisionIO.java
+++ src/main/java/xbot/common/subsystems/vision/AprilTagVisionIO.java
@@ -15,6 +15,7 @@ package xbot.common.subsystems.vision;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import org.littletonrobotics.junction.AutoLog;
 
 /**
@@ -26,13 +27,13 @@ public interface AprilTagVisionIO {
     class VisionIOInputs {
         public boolean connected = false;
         public TargetObservation latestTargetObservation =
-                new TargetObservation(new Rotation2d(), new Rotation2d());
+                new TargetObservation(0, new Rotation2d(), new Rotation2d(), new Transform3d());
         public PoseObservation[] poseObservations = new PoseObservation[0];
         public int[] tagIds = new int[0];
     }
 
     /** Represents the angle to a simple target, not used for pose estimation. */
-    record TargetObservation(Rotation2d tx, Rotation2d ty) {}
+    record TargetObservation(int fiducialId, Rotation2d tx, Rotation2d ty, Transform3d cameraToTarget) {}
 
     /** Represents a robot pose sample used for pose estimation. */
     record PoseObservation(
