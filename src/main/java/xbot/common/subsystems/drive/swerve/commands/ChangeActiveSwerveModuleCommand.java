aeb2ca321f3cecdc3f00df58afcf02d3c1493ee5
JohnGilb <johngilb@gmail.com>
2025-01-20 15:32:32 -0800

    Change active swerve module. Running the active swerve module must be done in competition code.

--- src/main/java/xbot/common/subsystems/drive/swerve/commands/ChangeActiveSwerveModuleCommand.java
+++ src/main/java/xbot/common/subsystems/drive/swerve/commands/ChangeActiveSwerveModuleCommand.java
@@ -1,4 +1,30 @@
 package xbot.common.subsystems.drive.swerve.commands;
 
-public class ChangeActiveSwerveModuleCommand {
+import xbot.common.command.BaseCommand;
+import xbot.common.subsystems.drive.BaseSwerveDriveSubsystem;
+
+import javax.inject.Inject;
+
+public class ChangeActiveSwerveModuleCommand extends BaseCommand {
+
+    final BaseSwerveDriveSubsystem drive;
+
+    @Inject
+    public ChangeActiveSwerveModuleCommand(BaseSwerveDriveSubsystem drive) {
+        this.drive = drive;
+    }
+
+    @Override
+    public void initialize() {
+        drive.setNextModuleAsActiveModule();
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
 }
