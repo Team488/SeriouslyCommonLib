a2b0f982d953c845dee33fbd214afed840149460
Stephen Just <stephenjust@gmail.com>
2025-01-21 17:30:19 -0800

    Feature/cancoder wpi units (#502)
    * Move CANCoder to use WPI Units library
    
    * Add config to cross-reference between javadocs of our dependencies

--- src/main/java/xbot/common/controls/io_inputs/XAbsoluteEncoderInputs.java
+++ src/main/java/xbot/common/controls/io_inputs/XAbsoluteEncoderInputs.java
@@ -1,11 +1,13 @@
 package xbot.common.controls.io_inputs;
 
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import org.littletonrobotics.junction.AutoLog;
 
 @AutoLog
 public class XAbsoluteEncoderInputs {
-    public double position;
-    public double absolutePosition;
-    public double velocity;
+    public Angle position;
+    public Angle absolutePosition;
+    public AngularVelocity velocity;
     public String deviceHealth;
 }
--- src/main/java/xbot/common/controls/io_inputs/XCANCoderInputs.java
+++ src/main/java/xbot/common/controls/io_inputs/XCANCoderInputs.java
@@ -3,6 +3,6 @@ package xbot.common.controls.io_inputs;
 import org.littletonrobotics.junction.AutoLog;
 
 @AutoLog
-public class XCANCoderInputs extends XAbsoluteEncoderInputs{
+public class XCANCoderInputs extends XAbsoluteEncoderInputs {
     public boolean hasResetOccurred;
 }
