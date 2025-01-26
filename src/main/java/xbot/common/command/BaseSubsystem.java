6b36ae8f324b6d9980f7b8c2a1448d1195548181
Stephen Just <stephenjust@gmail.com>
2025-01-25 22:05:22 -0800

    Document how DataFrameRefreshable works (#512)

--- src/main/java/xbot/common/command/BaseSubsystem.java
+++ src/main/java/xbot/common/command/BaseSubsystem.java
@@ -12,7 +12,7 @@ import xbot.common.advantage.DataFrameRefreshable;
 import xbot.common.properties.IPropertySupport;
 
 public abstract class BaseSubsystem extends SubsystemBase implements IPropertySupport, DataFrameRefreshable {
-    
+
     protected final Logger log;
     protected final AKitLogger aKitLog;
     protected final List<DataFrameRefreshable> dataFrameRefreshables = new ArrayList<>();
@@ -29,7 +29,18 @@ public abstract class BaseSubsystem extends SubsystemBase implements IPropertySu
     protected void registerDataFrameRefreshable(DataFrameRefreshable refreshable) {
         dataFrameRefreshables.add(refreshable);
     }
-    
+
+    /**
+     * This method is called on each {@link edu.wpi.first.wpilibj2.command.CommandScheduler} loop.
+     * @apiNote Subsystem periodic() methods are not executed in a predictable order.
+     * If you need to ensure that some state is available before any other subsystems are run,
+     * you should implement {@link #refreshDataFrame()} to update the state before the scheduler runs periodic() on all the subsystems.
+     */
+    @Override
+    public void periodic() {
+        super.periodic();
+    }
+
     @Override
     public void refreshDataFrame() {
         for (DataFrameRefreshable refreshable : dataFrameRefreshables) {
