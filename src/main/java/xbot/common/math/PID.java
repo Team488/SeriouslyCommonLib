cfa7b5c4d68d2a11bc1a9284a3a89ffa13055e45
kujo27 <johnkuang783@gmail.com>
2025-02-08 13:41:56 -0800

    fix derivative bug?

--- src/main/java/xbot/common/math/PID.java
+++ src/main/java/xbot/common/math/PID.java
@@ -2,6 +2,8 @@ package xbot.common.math;
 
 import xbot.common.controls.sensors.XTimer;
 
+import java.util.Optional;
+
 /**
  * This PID was extracted from WPILib. It has all the same functionality, but
  * does not run on its own separate thread.
@@ -21,25 +23,25 @@ public class PID
     private double m_maximumOutput = 1.0; // |maximum output|
     private double m_minimumOutput = -1.0; // |minimum output|
     private double m_result;
-    private double m_prevError;
+    private Optional<Double> m_prevError = Optional.empty();
     private double m_totalError;
     private double m_targetInputValue;
     private double m_currentInputValue;
-    
+
     private double m_derivativeValue;
     private boolean errorIsSmall = false;
     private boolean derivativeIsSmall = false;
     double errorTolerance = 0;
     double derivativeTolerance = 0;
-    
+
     private boolean checkErrorThreshold = false;
     private boolean checkDerivativeThreshold = false;
     private boolean checkTimeThreshold = false;
     private boolean waitingToStabilize = false;
     private double timeToleranceInSeconds = 0;
-    
+
     private double startTime = 0;
-    
+
     private boolean onTarget = false;
     private OffTargetReason offTargetReason = OffTargetReason.Unknown;
 
@@ -49,12 +51,12 @@ public class PID
      */
     public void reset()
     {
-        m_prevError = 0;
+        m_prevError = Optional.empty();
         m_totalError = 0;
         errorIsSmall = false;
         derivativeIsSmall = false;
     }
- 
+
     /**
      * Set tolerances for the PID system.
      * @param errorTolerance
@@ -73,14 +75,14 @@ public class PID
      *  but per 1/20th of a second.
      *
      *  e.g. if you wanted a minimum rotation speed of 5 degrees per second,
-     *  this tolerance would need to be 0.25.  
+     *  this tolerance would need to be 0.25.
      */
     public void setTolerances(double errorTolerance, double derivativeTolerance, double timeToleranceInSeconds) {
         this.errorTolerance = errorTolerance;
         this.derivativeTolerance = derivativeTolerance;
         this.timeToleranceInSeconds = timeToleranceInSeconds;
     }
-    
+
     /**
      * Controls whether or not the tolerances are checked as part of isOnTarget().
      */
@@ -89,10 +91,10 @@ public class PID
         checkDerivativeThreshold = checkDerivative;
         checkTimeThreshold = checkTime;
     }
-    
+
     /**
      * Calculates the output value given P,I,D, a process variable and a goal
-     *  
+     *
      * @param goal
      *            What value you are trying to achieve
      * @param current
@@ -104,7 +106,7 @@ public class PID
      * @param d
      *            Derivative response.
      * @param f
-     *            Feed-forward response            
+     *            Feed-forward response
      * @return The output value to achieve goal
      */
     public double calculate(double goal, double current,
@@ -132,8 +134,9 @@ public class PID
                 m_totalError = m_maximumOutput / i;
             }
         }
-
-        m_derivativeValue = m_error - m_prevError;
+        // if previous error is a value, do normal derivative calculation.
+        // otherwise derivative value should be 0
+        m_derivativeValue = m_prevError.map(aDouble -> m_error - aDouble).orElse(0.0);
         m_result = p * m_error + d * (m_derivativeValue) + f * goal;
 
         // If iZone is configured, but we are outside the zone, clear any accumulated I.
@@ -144,7 +147,7 @@ public class PID
         if(iZone <= 0 || Math.abs(m_error) < iZone) {
             m_result +=  i * m_totalError;
         }
-        m_prevError = m_error;
+        m_prevError = Optional.of(m_error);
 
         if (m_result > m_maximumOutput)
         {
@@ -154,18 +157,18 @@ public class PID
             m_result = m_minimumOutput;
         }
         result = m_result;
-        
+
         errorIsSmall = checkErrorThreshold && Math.abs(m_targetInputValue - m_currentInputValue) < errorTolerance;
         derivativeIsSmall = checkDerivativeThreshold && Math.abs(m_derivativeValue) < derivativeTolerance;
-        
+
         checkIsOnTarget();
-        
+
         return result;
     }
 
     /**
      * Calculates the output value given P,I,D, a process variable and a goal
-     * 
+     *
      * @param goal
      *            What value you are trying to achieve
      * @param current
@@ -184,10 +187,10 @@ public class PID
             double p, double i, double d, double f) {
         return calculate(goal, current, p, i, d, f, 0);
     }
-    
+
     /**
      * Calculates the output value given P,I,D, a process variable and a goal
-     * 
+     *
      * @param goal
      *            What value you are trying to achieve
      * @param current
@@ -204,15 +207,15 @@ public class PID
             double p, double i, double d) {
         return calculate(goal, current, p, i, d, 0);
     }
-    
+
     /**
      * With the addition of a time threshold, "onTarget" needs to be updated every tick, since
      * isOnTarget() should return true if all conditions are met EVEN IF it's the first time
      * being called.
-     * 
+     *
      * While that's unlikely due to our command infrastructure, it's still possible, and the PID
      * class should be able to handle that scenario.
-     * 
+     *
      * As a result, this checkIsOnTarget method was added, which does all the onTarget calculations
      * during every calculate() call. the isOnTarget() method just returns a stored class-level variable.
      */
@@ -225,11 +228,11 @@ public class PID
             offTargetReason = OffTargetReason.OffTargetNotConfigured;
             return;
         }
-        
+
         // Assume we are on target, and then look for reasons we may not be.
         offTargetReason = OffTargetReason.OnTarget;
         onTarget = true;
-        
+
         if (checkDerivativeThreshold) {
             onTarget &= derivativeIsSmall;
             offTargetReason = derivativeIsSmall ? offTargetReason : OffTargetReason.DerivativeTooLarge;
@@ -237,8 +240,8 @@ public class PID
         if (checkErrorThreshold) {
             onTarget &= errorIsSmall;
             offTargetReason = errorIsSmall ? offTargetReason : OffTargetReason.ErrorTooLarge;
-        }        
-        
+        }
+
         if (checkTimeThreshold) {
             if(onTarget){
                 if(waitingToStabilize == false){
@@ -256,13 +259,13 @@ public class PID
             }
         }
     }
-    
+
     public boolean isOnTarget() {
         return onTarget;
     }
 
     /**
-     * Returns an enum explaining the reason the PID is not on target. 
+     * Returns an enum explaining the reason the PID is not on target.
      * It has the following priority list if there are multiple reasons (higher ones overrule lower ones):
      * - Too much error
      * - Too much derivative
