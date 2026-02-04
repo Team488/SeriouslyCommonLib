a08e6f6108c3e5f20fd80b1b0e8f70457d1de797
Rongrui Zhu <ronrzhu@gmail.com>
2026-02-03 19:33:04 -0800

    Create MotorInternalPIDHelper.java

---
+++ src/main/java/xbot/common/simulation/MotorInternalPIDHelper.java
@@ -0,0 +1,41 @@
+package xbot.common.simulation;
+
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Rotations;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import xbot.common.controls.actuators.mock_adapters.MockCANMotorController;
+import xbot.common.math.PIDManager;
+
+public class MotorInternalPIDHelper {
+    public static void updateInternalPID(MockCANMotorController motor, PIDManager pidManager) {
+        updateInternalPIDWithGravity(motor, pidManager, 0.0);
+    }
+
+    public static void updateInternalPIDWithGravity(
+            MockCANMotorController motor, PIDManager pidManager, double gravityFeedForward) {
+        // based on the motor state, potentially run internal PID if need be
+        if (motor.getControlMode() == MockCANMotorController.ControlMode.Position) {
+            // run a simple pid to mimic the internal pid of the motor controller
+            var targetPosition = motor.getTargetPosition();
+            var currentPosition = motor.getPosition();
+            var output = pidManager.calculate(targetPosition.in(Rotations), currentPosition.in(Rotations))
+                    + gravityFeedForward;
+            motor.setPower(output);
+        } else {
+            // Duty Cycle
+            pidManager.reset();
+        }
+    }
+
+    public static void updateInternalPIDWithVelocity(
+            MockCANMotorController motor, PIDManager pidManager, AngularVelocity targetVelocity) {
+        if (motor.getControlMode() == MockCANMotorController.ControlMode.Velocity) {
+            var currentVelocity = motor.getVelocity();
+            var output = pidManager.calculate(targetVelocity.in(RPM), currentVelocity.in(RPM));
+            motor.setPower(output);
+        } else {
+            pidManager.reset();
+        }
+    }
+}
