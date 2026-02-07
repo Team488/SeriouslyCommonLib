46bd0ac221133d2f45542d80865c1ee3458d2d9e
Victor C <vich2021@gmail.com>
2026-02-07 10:20:40 -0800

    Added methods to channels (#623)
    * Added methods to channels
    
    Added new types of monitors such as voltage, temperature, and total energy.
    
    * Fixed Formatting Issues
    
    ---------
    
    Co-authored-by: Stephen Just <stephenjust@gmail.com>

--- src/main/java/xbot/common/controls/sensors/wpi_adapters/PowerDistributionPanelWPIAdapter.java
+++ src/main/java/xbot/common/controls/sensors/wpi_adapters/PowerDistributionPanelWPIAdapter.java
@@ -7,9 +7,9 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import xbot.common.controls.sensors.XPowerDistributionPanel;
 
 public class PowerDistributionPanelWPIAdapter extends XPowerDistributionPanel {
-    
+
     private PowerDistribution pdp;
-    
+
     @AssistedFactory
     public abstract static class PowerDistributionPanelWPIAdapaterFactory implements XPowerDistributionPanelFactory {
         @Override
@@ -20,9 +20,39 @@ public class PowerDistributionPanelWPIAdapter extends XPowerDistributionPanel {
     public PowerDistributionPanelWPIAdapter() {
         pdp = new PowerDistribution();
     }
-    
+
     @Override
     public double getCurrent(int channel) {
         return pdp.getCurrent(channel);
+    }
+
+    @Override
+    public double getVoltage() {
+        return pdp.getVoltage();
+    }
+
+    @Override
+    public double getTemperature() {
+        return pdp.getTemperature();
+    }
+
+    @Override
+    public double getTotalCurrent() {
+        return pdp.getTotalCurrent();
+    }
+
+    @Override
+    public double getTotalPower() {
+        return pdp.getTotalPower();
+    }
+
+    @Override
+    public double getTotalEnergy() {
+        return pdp.getTotalEnergy();
+    }
+
+    @Override
+    public double getModule() {
+        return pdp.getModule();
     }
 }
