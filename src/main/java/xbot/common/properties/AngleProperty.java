c449056c24c64f5c490b55b849d7c5d10294cca2
Rongrui Zhu <ronrzhu@gmail.com>
2026-02-21 15:14:44 -0800

    Remove unused imports

--- src/main/java/xbot/common/properties/AngleProperty.java
+++ src/main/java/xbot/common/properties/AngleProperty.java
@@ -1,14 +1,8 @@
 package xbot.common.properties;
 
-import java.util.function.Consumer;
-import org.littletonrobotics.junction.LogTable;
-import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.inputs.LoggableInputs;
-
 import edu.wpi.first.units.AngleUnit;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.MutAngle;
-import xbot.common.logging.Pluralizer;
 
 /**
  * This manages an Angle in the property system.
--- src/main/java/xbot/common/properties/AngularVelocityProperty.java
+++ src/main/java/xbot/common/properties/AngularVelocityProperty.java
@@ -3,12 +3,6 @@ package xbot.common.properties;
 import edu.wpi.first.units.AngularVelocityUnit;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.MutAngularVelocity;
-import org.littletonrobotics.junction.LogTable;
-import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.inputs.LoggableInputs;
-import xbot.common.logging.Pluralizer;
-
-import java.util.function.Consumer;
 
 /**
  * This manages an AngleVelocity in the property system.
