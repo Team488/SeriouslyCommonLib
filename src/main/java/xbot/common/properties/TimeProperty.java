627620f11e55f924600764540b17e57035c5b945
Stephen Just <stephenjust@gmail.com>
2026-07-29 20:10:47 -0700

    Remove use of mutable measures
    WPILib 2027 removes the concept of mutable measures, so now is a good time to clean these up.

--- src/main/java/xbot/common/properties/TimeProperty.java
+++ src/main/java/xbot/common/properties/TimeProperty.java
@@ -1,7 +1,6 @@
 package xbot.common.properties;
 
 import edu.wpi.first.units.TimeUnit;
-import edu.wpi.first.units.measure.MutTime;
 import edu.wpi.first.units.measure.Time;
 
 /**
@@ -9,7 +8,7 @@ import edu.wpi.first.units.measure.Time;
  *
  * @author Alex
  */
-public class TimeProperty extends MeasureProperty<Time, MutTime, TimeUnit> {
+public class TimeProperty extends MeasureProperty<Time, TimeUnit> {
     public TimeProperty(String prefix, String name, Time defaultValue, XPropertyManager manager) {
         super(prefix, name, defaultValue, manager);
     }
